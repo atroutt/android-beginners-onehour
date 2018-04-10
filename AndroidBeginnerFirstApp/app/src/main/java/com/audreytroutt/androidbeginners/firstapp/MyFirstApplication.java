b3e562e25bbf6ce605bf73b993788678ca78f4c7
Audrey Troutt <audrey.troutt@gmail.com>
2018-04-10 16:34:20 -0400

    Get rid of the launch toast

--- AndroidBeginnerFirstApp/app/src/main/java/com/audreytroutt/androidbeginners/firstapp/MyFirstApplication.java
+++ AndroidBeginnerFirstApp/app/src/main/java/com/audreytroutt/androidbeginners/firstapp/MyFirstApplication.java
@@ -1,14 +1,17 @@
 package com.audreytroutt.androidbeginners.firstapp;
 
 import android.app.Application;
-import android.widget.Toast;
+import android.util.Log;
 
 public class MyFirstApplication extends Application {
+
+    private static final String TAG = "MyFirstApplication";
 
     @Override
     public void onCreate() {
         super.onCreate();
-        // You can delete this Toast if you want :)
-        Toast.makeText(this, "MyFirstApplication has been created!", Toast.LENGTH_LONG).show();
+        // If you want to see this log message, open "Logcat" here in Android Studio
+        // TIP: open Logcat from the button in the bottom of the Android Studio window OR go to View -> Tool Windows -> Logcat OR cmd+6 on a Mac
+        Log.d(TAG, "MyFirstApplication has been created!");
     }
 }
